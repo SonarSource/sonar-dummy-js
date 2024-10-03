a09473e144e634bddc6abece529f527dd33d2891
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2024-10-03 13:52:31 +0200

    BUILD-6479 Add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2014-2024 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+function helloNpm() {
+    return "hello NPM"
+}
+
+module.exports = helloNpm
