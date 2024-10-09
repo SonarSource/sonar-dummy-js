ca5b74b6c30208ecd7335fa8d593ea4160cf7549
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2024-10-09 08:56:36 +0200

    BUILD-5928 Demonstrate improvements

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-const helloNpm = require('../index'); // Import the function
+const helloNpm = require('../src/index'); // Import the function
 
 test('returns hello NPM', () => {
     expect(helloNpm()).toBe('hello NPM');  // Check if it returns the correct string
