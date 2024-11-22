ac3a67e262c5414fc615a2d6c14ad817a8a64aed
Samir M <samir.marini@sonarsource.com>
2024-11-22 13:45:42 +0100

    test

---
+++ src/npm/env_setup_temp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+export ARTIFACTORY_URL="url"
+export ARTIFACTORY_DEPLOY_ACCESS_TOKEN="token"
+export SONAR_HOST_URL="sonar_url"
+export SONAR_TOKEN="sonar_token"
+export PIPELINE_ID="12345"
+export GITHUB_BASE_BRANCH="master"
+export BUILD_NUMBER="1"
+export GIT_SHA1="sha1"
+export GITHUB_REPO="repo"
