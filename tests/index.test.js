783e96fc2befb7677e2c9bea285b356f641ed928
Samir M <samir.marini@sonarsource.com>
2024-11-22 13:45:42 +0100

    BUILD-6984 test new scripts

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
-const helloNpm = require('../src/index'); // Import the function
-
+function helloNpm() {
+  return 'hello NPM';
+}
 test('returns hello NPM', () => {
     expect(helloNpm()).toBe('hello NPM');  // Check if it returns the correct string
 });
